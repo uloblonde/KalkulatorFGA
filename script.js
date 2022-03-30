59da831259a44e91fca907d0b08096cc32fbccce
Robert Mahendra <43535409+uloblonde@users.noreply.github.com>
2022-03-31 02:13:20 +0700

    Menambahkan function inputan dan penggunaan operator

---
+++ script.js
@@ -0,0 +1,12 @@
+function masukin(valuenya){
+    ikiform.field.value = ikiform.field.value += valuenya
+  }
+  function samadengan(){
+    ikiform.field.value = eval(ikiform.field.value)
+  }
+  function clear(){
+
+  }
+  function hapus(){
+
+  }
